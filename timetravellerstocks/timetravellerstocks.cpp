e2767754f53de612c10fb73cb041c3f3590f9280
BOLLAM RAJA SHEKAR <bs.raja@samsung.com>
2021-10-06 19:18:52 +0900

    time travellers stocks

---
+++ timetravellerstocks/timetravellerstocks.cpp
@@ -0,0 +1,116 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <stdio.h>
+
+#define CMD_INIT 1
+#define CMD_BUY 2
+#define CMD_SELL 3
+#define CMD_CANCEL 4
+#define CMD_BEST_PROFIT 5
+
+void init()
+{
+}
+
+int buy(int mNumber, int mStock, int mQuantity, int mPrice)
+{
+    return 0;
+}
+
+int sell(int mNumber, int mStock, int mQuantity, int mPrice)
+{
+    return 0;
+}
+
+void cancel(int mNumber)
+{
+}
+
+int bestProfit(int mStock)
+{
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+static bool run()
+{
+    int numQuery;
+
+    int mNumber, mStock, mQuantity, mPrice;
+
+    int userAns, ans;
+
+    bool isCorrect = false;
+
+    scanf("%d", &numQuery);
+
+    for (int i = 0; i < numQuery; ++i)
+    {
+        int cmd;
+        scanf("%d", &cmd);
+        switch (cmd)
+        {
+        case CMD_INIT:
+            init();
+            isCorrect = true;
+            break;
+        case CMD_BUY:
+            scanf("%d %d %d %d", &mNumber, &mStock, &mQuantity, &mPrice);
+            userAns = buy(mNumber, mStock, mQuantity, mPrice);
+            scanf("%d", &ans);
+            if (userAns != ans)
+            {
+                isCorrect = false;
+            }
+            break;
+        case CMD_SELL:
+            scanf("%d %d %d %d", &mNumber, &mStock, &mQuantity, &mPrice);
+            userAns = sell(mNumber, mStock, mQuantity, mPrice);
+            scanf("%d", &ans);
+            if (userAns != ans)
+            {
+                isCorrect = false;
+            }
+            break;
+        case CMD_CANCEL:
+            scanf("%d", &mNumber);
+            cancel(mNumber);
+            break;
+        case CMD_BEST_PROFIT:
+            scanf("%d", &mStock);
+            userAns = bestProfit(mStock);
+            scanf("%d", &ans);
+            if (userAns != ans)
+            {
+                isCorrect = false;
+            }
+            break;
+        default:
+            isCorrect = false;
+            break;
+        }
+    }
+
+    return isCorrect;
+}
+
+int main()
+{
+    setbuf(stdout, NULL);
+    //freopen("sample_input.txt", "r", stdin);
+
+    int T, MARK;
+    scanf("%d %d", &T, &MARK);
+
+    for (int tc = 1; tc <= T; tc++)
+    {
+        int score = run() ? MARK : 0;
+        printf("#%d %d\n", tc, score);
+    }
+
+    return 0;
+}
